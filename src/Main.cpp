e2db8a0b86954449ff90cf1f5b8e6e361274f4ca
Treidex <treidexsemextetry@gmail.com>
2021-03-17 17:04:55 -0700

    ok, time for game

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,16 @@
 #include "olcPixelGameEngine.h"
 #include "olcPGEX_Graphics2D.h"
 
+
+#define WIDTH 600
+#define HEIGHT 900
+
+namespace Math
+{
+	constexpr float Pi { 3.1415926535f };
+	constexpr auto Abs = std::fabsf;
+};
+
 struct Player
 {
 	union
@@ -12,7 +22,7 @@ struct Player
 	};
 
 	float Speed { 69 };
-	float Drag { 1 };
+	float Drag { 0 };
 	olc::vf2d Velocity {};
 
 	float Radius { 25 };
@@ -22,31 +32,42 @@ class Game: public olc::PixelGameEngine
 {
 private:
 	Player m_Player {};
-
-	__declspec(property(get = ScreenWidth)) int m_Width;
-	__declspec(property(get = ScreenHeight)) int m_Height;
+	olc::vf2d Gravity { 0, Math::Pi };
+	float WallBounce { 0.42f };
 public:
 	Game()
 	{ sAppName = "Bally"; }
 
 	virtual bool OnUserCreate() override
-	{ return true; }
+	{
+		m_Player.Pos = { (float)WIDTH / 2, (float)HEIGHT / 2 };
+		return true;
+	}
 
 	virtual bool OnUserUpdate(float deltaTime) override
 	{
-		if (GetKey(olc::Key::UP).bHeld)
-			m_Player.Velocity.y -= m_Player.Speed * deltaTime;
-		if (GetKey(olc::Key::DOWN).bHeld)
-			m_Player.Velocity.y += m_Player.Speed * deltaTime;
-		if (GetKey(olc::Key::LEFT).bHeld)
-			m_Player.Velocity.x -= m_Player.Speed * deltaTime;
-		if (GetKey(olc::Key::RIGHT).bHeld)
-			m_Player.Velocity.x += m_Player.Speed * deltaTime;
-
+		if (m_Player.Y + m_Player.Radius > HEIGHT)
+		{
+			m_Player.Y = HEIGHT - m_Player.Radius;
+			m_Player.Velocity.y = -Math::Abs(m_Player.Velocity.y);
+		}
+		
+		if (m_Player.X + m_Player.Radius > WIDTH)
+		{
+			m_Player.X = WIDTH - m_Player.Radius;
+			m_Player.Velocity.x = -Math::Abs(m_Player.Velocity.x) * WallBounce;
+		}
+		else if (m_Player.X - m_Player.Radius < 0)
+		{
+			m_Player.X = m_Player.Radius;
+			m_Player.Velocity.x = Math::Abs(m_Player.Velocity.x) * WallBounce;
+		}
+		
+		m_Player.Velocity += Gravity * deltaTime;
 		m_Player.Pos += m_Player.Velocity;
 		m_Player.Velocity -= m_Player.Velocity * m_Player.Drag * deltaTime;
 
-		FillRect({ 0, 0 }, olc::vf2d(m_Width, m_Height), olc::PixelF(0.25f, 0.25f, 0.25f));
+		Clear(olc::PixelF(0.25f, 0.25f, 0.25f));
 		FillCircle(m_Player.Pos, m_Player.Radius, olc::PixelF(0, 0.5f, 0.6f));
 		return true;
 	}
@@ -56,7 +77,7 @@ public:
 int main()
 {
 	Game game;
-	if (game.Construct(600, 900, 1, 1))
+	if (game.Construct(WIDTH, HEIGHT, 1, 1))
 		game.Start();
 
 	return 0;
