73bea695d5fe67b9b7568c9539179794e86e037c
Treidex <treidexsemextetry@gmail.com>
2021-03-17 17:36:33 -0700

    pads!

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,39 +7,81 @@
 #define WIDTH 600
 #define HEIGHT 900
 
-namespace Math
+#define POS_UNION(x, y) union { Vec2f Pos { x, y }; struct { float X, Y; }; }
+#define SIZE_UNION(w, h) union { Vec2f Size { w, h }; struct { float Width, Height; }; }
+
+namespace
 {
-	constexpr float Pi { 3.1415926535f };
-	constexpr auto Abs = std::fabsf;
-};
+	olc::PixelGameEngine *Instance;
+
+	using Vec2i = olc::vi2d;
+	using Vec2f = olc::vf2d;
+	using olc::PixelF;
+
+	using Transform = olc::GFX2D::Transform2D;
+
+	namespace Math
+	{
+		constexpr float Pi { 3.1415926535f };
+		constexpr auto Abs = std::fabsf;
+	};
+}
 
 struct Player
 {
-	union
-	{
-		olc::vf2d Pos {};
-		struct { float X, Y; };
-	};
-
+	POS_UNION(0, 0);
 	float Speed { 69 };
 	float Drag { 0 };
-	olc::vf2d Velocity {};
+	Vec2f Velocity {};
 
 	float Radius { 25 };
+};
+
+struct Pad
+{
+	POS_UNION(0, 0);
+	SIZE_UNION(100, 30);
+	float Rot {};
+
+	virtual void Draw()
+	{ Instance->FillRect(Pos, Size, PixelF(0, 1, 0)); }
+
+	virtual void Update(float in, float deltaTime) = 0;
+};
+
+struct TurnPad: public Pad
+{
+	float RotSpeed { 5 };
+
+	virtual void Update(float in, float deltaTime) override
+	{
+		Rot += RotSpeed * deltaTime;
+	}
 };
 
 class Game: public olc::PixelGameEngine
 {
 private:
 	Player m_Player {};
-	olc::vf2d Gravity { 0, Math::Pi };
+	Vec2f Gravity { 0, Math::Pi };
 	float WallBounce { 0.42f };
+	float GroundBounce { 0.88f };
+
+	std::vector<Pad *> m_Pads {};
 public:
 	Game()
-	{ sAppName = "Bally"; }
+	{
+		Instance = this;
+		sAppName = "Bally";
+	}
 
 	virtual bool OnUserCreate() override
 	{
+		olc::GFX2D s;
+
+		TurnPad *pad = new TurnPad;
+		pad->Pos = { (float)WIDTH / 2, (float)HEIGHT / 2 };
+		m_Pads.push_back(pad);
 		m_Player.Pos = { (float)WIDTH / 2, (float)HEIGHT / 2 };
 		return true;
 	}
@@ -49,9 +91,9 @@ public:
 		if (m_Player.Y + m_Player.Radius > HEIGHT)
 		{
 			m_Player.Y = HEIGHT - m_Player.Radius;
-			m_Player.Velocity.y = -Math::Abs(m_Player.Velocity.y);
+			m_Player.Velocity.y = -Math::Abs(m_Player.Velocity.y) * GroundBounce;
 		}
-		
+
 		if (m_Player.X + m_Player.Radius > WIDTH)
 		{
 			m_Player.X = WIDTH - m_Player.Radius;
@@ -62,17 +104,19 @@ public:
 			m_Player.X = m_Player.Radius;
 			m_Player.Velocity.x = Math::Abs(m_Player.Velocity.x) * WallBounce;
 		}
-		
+
 		m_Player.Velocity += Gravity * deltaTime;
 		m_Player.Pos += m_Player.Velocity;
 		m_Player.Velocity -= m_Player.Velocity * m_Player.Drag * deltaTime;
 
-		Clear(olc::PixelF(0.25f, 0.25f, 0.25f));
-		FillCircle(m_Player.Pos, m_Player.Radius, olc::PixelF(0, 0.5f, 0.6f));
+		Clear(PixelF(0.25f, 0.25f, 0.25f));
+		FillCircle(m_Player.Pos, m_Player.Radius, PixelF(0, 0.5f, 0.6f));
+
+		for (Pad *pad : m_Pads)
+			pad->Draw();
 		return true;
 	}
 };
-
 
 int main()
 {
